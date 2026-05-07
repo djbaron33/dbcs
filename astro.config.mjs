89993403b79dea524f8ce751d61994cc97fce0d6
Devin Baron <devinbaron@Devins-MacBook-Pro-2.local>
2026-05-06 20:39:45 -0500

    configure custom domain paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
-  site: "https://djbaron33.github.io",
-  base: "/dbcs/",
+  site: "https://devinbaron.com",
   integrations: [mdx()],
 });
