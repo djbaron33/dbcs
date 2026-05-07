fdc06683588a25efb7f6eba87d56b0eae7cf5a97
Devin Baron <devinbaron@Devins-MacBook-Pro-2.local>
2026-05-06 19:48:46 -0500

    fix github pages asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://djbaron33.github.io",
+  base: "/dbcs/",
   integrations: [mdx()],
 });
